9cb072e856ec15e8036cfd7b4377f3bb0bd28b1b
Bertrik Sikken <bertrik@sikken.nl>
2025-03-23 23:38:55 +0100

    Update LoRa initialisation.

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -1,9 +1,11 @@
 #include <Arduino.h>
-#include <SPI.h>
 #include <mbedtls/aes.h>
 
 #include <MiniShell.h>
+#include <SPI.h>
 #include <LoRa.h>
+
+#define printf Serial.printf
 
 static MiniShell shell(&Serial);
 static uint32_t packet_cnt = 0;
@@ -28,12 +30,42 @@ static int do_reboot(int argc, char *argv[])
     return 0;
 }
 
+static void printhex(const uint8_t *data, size_t len)
+{
+    for (int i = 0; i < len; i++) {
+        if ((i % 16) == 0) {
+            printf("\n%04X:", i);
+        }
+        printf(" %02X", data[i]);
+    }
+    printf("\n");
+}
+
+static bool lora_init(void)
+{
+    SPI.begin(LORA_SCK, LORA_MISO, LORA_MOSI, LORA_CS);
+    LoRa.setSPI(SPI);
+    LoRa.setSPIFrequency(100000L);
+    LoRa.setPins(LORA_CS, LORA_RST, LORA_IRQ);
+    bool result = LoRa.begin(869525000L);
+    if (result) {
+        LoRa.setSpreadingFactor(11);
+        LoRa.setSignalBandwidth(250E3);
+        LoRa.setCodingRate4(5);
+        LoRa.setSyncWord(0x2B);
+        LoRa.setPreambleLength(16);
+        LoRa.enableCrc();
+        LoRa.disableInvertIQ();  // meshtastic doesn't specify! 
+    }
+    return result;
+}
+
 static size_t put_u32_le(uint8_t *buffer, uint32_t value)
 {
-    *buffer++ = (value >> 24) & 0xFF;
-    *buffer++ = (value >> 16) & 0xFF;
-    *buffer++ = (value >> 8) & 0xFF;
     *buffer++ = (value >> 0) & 0xFF;
+    *buffer++ = (value >> 8) & 0xFF;
+    *buffer++ = (value >> 16) & 0xFF;
+    *buffer++ = (value >> 24) & 0xFF;
     return 4;
 }
 
@@ -45,9 +77,7 @@ static size_t put_u8(uint8_t *buffer, uint8_t value)
 
 static size_t fill_header(uint8_t *buffer, uint32_t source, uint32_t packet_id)
 {
-    size_t index = 0;
     uint8_t *p = buffer;
-
     p += put_u32_le(p, 0xFFFFFFFF);     // destination
     p += put_u32_le(p, source);
     p += put_u32_le(p, packet_id);
@@ -55,7 +85,6 @@ static size_t fill_header(uint8_t *buffer, uint32_t source, uint32_t packet_id)
     p += put_u8(p, 0);          // hash
     p += put_u8(p, 0);          // next-hop
     p += put_u8(p, 0);          // relay-node
-
     return p - buffer;
 }
 
@@ -89,6 +118,9 @@ static void send_data(const uint8_t *data, size_t data_len, uint32_t source, uin
     uint8_t nonce[16];
     uint8_t *p = packet;
 
+    printf("Sending data:");
+    printhex(data, data_len);
+
     // build header
     p += fill_header(p, source, packet_id);
 
@@ -98,34 +130,44 @@ static void send_data(const uint8_t *data, size_t data_len, uint32_t source, uin
 
     // send buffer
     size_t len = p - packet;
+    printf("Encoded buffer:");
+    printhex(packet, len);
     LoRa.beginPacket(false);
     LoRa.write(data, len);
-    LoRa.end();
+    LoRa.endPacket();
 }
 
-static int do_send(int argc, char *argv[])
+static int do_init(int argc, char *argv[])
 {
+    bool result = lora_init();
+    printf("LoRa init: %s\n", result ? "OK" : "FAILED");
     return 0;
 }
 
 static int do_text(int argc, char *argv[])
 {
-    if (argc < 2) {
-        return -1;
+    const char *text = "Hallo dit is een testbericht";
+    if (argc > 1) {
+        text = argv[1];
     }
 
-    const char *text = argv[1];
     size_t len = strlen(text);
-
-    uint32_t source = 3663960916 + 1;
+    uint32_t source = 0xDA639B54 + 1;
     uint32_t packet_id = packet_cnt++;
-    send_data((uint8_t *) text, len, source, packet_id);
+    send_data((const uint8_t *) text, len, source, packet_id);
 
     return 0;
 }
 
+static int do_dump(int argc, char *argv[])
+{
+    LoRa.dumpRegisters(Serial);
+    return 0;
+}
+
 const cmd_t commands[] = {
-    { "send", do_send, "<hex> send bytes" },
+    { "init", do_init, "Initialize hardware"},
+    { "dump", do_dump, "Dump registers"},
     { "text", do_text, "<text> send a text message" },
     { "reboot", do_reboot, "Reboot" },
     { "help", do_help, "Show help" },
@@ -142,19 +184,7 @@ void setup(void)
 {
     Serial.begin(115200);
     Serial.println("Hello ESP32!");
-
-    pinMode(LORA_RST, OUTPUT);
-    digitalWrite(LORA_RST, 1);
-    delay(100);
-    digitalWrite(LORA_RST, 0);
-
-    SPI.begin(LORA_SCK, LORA_MISO, LORA_MOSI, LORA_CS);
-    LoRa.begin(869525000L);
-    LoRa.setSpreadingFactor(11);
-    LoRa.setSignalBandwidth(250000L);
-    LoRa.setCodingRate4(5);
-    LoRa.setSyncWord(0x2B);
-    LoRa.setPreambleLength(16);
+    lora_init();
 }
 
 void loop(void)
